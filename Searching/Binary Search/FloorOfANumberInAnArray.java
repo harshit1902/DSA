ca01de7a4e63e27b43d19e357c07a11f56105d07
Harshit Goyal <hg6706@gmail.com>
2021-08-20 22:02:36 +0530

    Ceiling and floor in an array

---
+++ Searching/Binary Search/FloorOfANumberInAnArray.java
@@ -0,0 +1,45 @@
+package dev.harshit;
+
+public class FloorOfANumberInAnArray {
+
+    public static void main(String[] args) {
+
+        int[] nums={1,2,3,6,9,18,22};
+        int[] numsDesc={22,18,9,6,3,2,1};
+        int target=15;
+
+        System.out.println(floor(numsDesc,target));
+        System.out.println(floor(nums,target));
+
+    }
+
+    static int floor(int[]arr,int target){
+
+
+        int start=0;
+        int end=arr.length-1;
+        int mid;
+        boolean isAsc=arr[arr.length-1]>arr[0];
+        while(start<=end){
+
+            mid=start+(end-start)/2;
+            if (arr[mid] == target) return arr[mid];
+            if(isAsc) {
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }else {
+                if(target<arr[mid]){
+                    start=mid+1;
+                }else {
+                    end=mid-1;
+                }
+            }
+        }
+
+        return isAsc?arr[end]:arr[start];
+    }
+
+}
